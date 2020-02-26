98cdef0f216a8de88ce61901193eef76e651bca3
bryantinge <34388345+bryantinge@users.noreply.github.com>
2020-02-26 17:57:53 -0500

    Update app.py

--- app.py
+++ app.py
@@ -52,7 +52,6 @@ def login():
     url_args = '&'.join([f'{key}={quote(val)}'
                          for key, val in auth_query_parameters.items()])
     auth_url = f'{SPOTIFY_AUTH_URL}/?{url_args}'
-    print(REDIRECT_URI)
     return redirect(auth_url)
 
 
